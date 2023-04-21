4997be124da8bdeeb9646a1db3cffa499a04bc10
davidreyesd13 <davidreyesd13@gmail.com>
2023-04-21 14:53:23 -0600

    Ejercicio 2

--- Lab1P3_DavidReyes.cpp
+++ Lab1P3_DavidReyes.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int opcion;
+	int opcion,n,x,s,a,b,p,m,j;
+	
 	cout << "1. Ejercicio 1\n2.Ejercicio 2\n0.Salir\n";
 	cin >> opcion;
 	while (opcion!=0) {
 		switch (opcion) {
 			case 1:
-			
+				 n = 0;
+				 cout << "Ingrese el valor de N: ";
+
 			break;
 				case 2:
-
+					m = 0;
+					cout << "Ingrese P: " << endl;
+					cin >> p;
+					cout << "Ingrese A: " << endl;
+					cin >> a;
+					cout << "Ingrese B: " << endl;
+					cin >> b;
+					while (b>0&&p>0) {
+						j = a / b;
+						m += j;
+						b--;
+						p--;
+					}
+					cout << "M= " << m << endl;
 					break;
 						default:
 						cout << "Opcion no valida";
@@ -18,6 +34,4 @@ int main(){
 		cout << "1. Ejercicio 1\n2.Ejercicio 2\n0.Salir\n";
 		cin >> opcion;
 	}
-	cout << "Buen dia";
-	
 }
