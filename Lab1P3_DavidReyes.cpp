2958707fb0c6f648bc93a43781434d46418b9e91
davidreyesd13 <davidreyesd13@gmail.com>
2023-04-21 14:12:09 -0600

    Menu

---
+++ Lab1P3_DavidReyes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int opcion;
+	cout << "1. Ejercicio 1\n2.Ejercicio 2\n0.Salir\n";
+	cin >> opcion;
+	while (opcion!=0) {
+		switch (opcion) {
+			case 1:
+			
+			break;
+				case 2:
+
+					break;
+						default:
+						cout << "Opcion no valida";
+		}
+		cout << "1. Ejercicio 1\n2.Ejercicio 2\n0.Salir\n";
+		cin >> opcion;
+	}
+	cout << "Buen dia";
+	
+}
